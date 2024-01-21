cafe56d78559636ba3918c3ad3fd873a1fceaa73
User <user@Lihis-MacBook-Pro.local>
2024-01-22 00:36:59 +0200

    Wet run :)

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Show me what you got!!!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
